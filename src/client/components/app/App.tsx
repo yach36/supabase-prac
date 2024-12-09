b36b4bf063c93b34190a9181713603c9e8facbca
yasuda.chuma <86759515+yach36@users.noreply.github.com>
2024-12-10 01:02:20 +0900

    initial commit

---
+++ src/client/components/app/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p>App Component</p>
+    </>
+  );
+}
+
+export default App;
---
+++ vitest.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react-swc";
+
+// https://vite.dev/config/
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./setupTests.ts",
+  },
+});
